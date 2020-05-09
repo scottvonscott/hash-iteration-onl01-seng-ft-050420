56c228fbb4b0910f8c8bff89fc0cbc07e9bbe190
Scott  Keplinger <skeplin17@gmail.com>
2020-05-09 21:21:45 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,7 +15,7 @@ birthday_kids.each do |kids_name, age|
   if age <= 12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   else
-    puts "You are too old for this"
+    puts "You are too old for this."
 end
 end
 end

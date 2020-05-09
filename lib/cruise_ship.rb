41f0de91a23cf8bf3d44b7f698b3633545d30832
Scott  Keplinger <skeplin17@gmail.com>
2020-05-09 21:07:45 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,7 +7,8 @@
 # }
 
 def select_winner(passengers)
-  passengers.each {|suite, name| if suite == :suite_a && name.start_with?("A")}
-    winner = name
+  passengers.each do |suite, name| 
+    if suite == :suite_a && name.start_with?("A")}
+      winner = name
     end
   end
